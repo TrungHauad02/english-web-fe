63b60f33f880eaf9c377c8bb870bd79a4332ffbf
Nguyễn Trung Hậu <trunghauad02@gmail.com>
2024-09-09 17:12:41 +0700

    List Topic

--- src/components/student/vocabulary/ListTopic.jsx
+++ src/components/student/vocabulary/ListTopic.jsx
@@ -1,10 +1,54 @@
-import { Stack } from "@mui/material";
+import { Stack, Typography } from "@mui/material";
+import MainPicture from "./MainPicture";
+import ReactPaginate from 'react-paginate';
+import ListTopicContent from "./ListTopicContent";
+import { useState } from "react";
+import './ListTopic.css';
+
 function ListTopic() {
-  return (
-    <Stack spacing={2}>
-      <h4>Topic</h4>
-    </Stack>
-  );
+    const topicsPerPage = 6;
+    const [currentPage, setCurrentPage] = useState(0);
+
+    const listTopic = [
+        {title: "Environment1", description: "This lesson we will discussion about environment, pollution and global warnming", img: "/environment.png"},
+        {title: "Environment2", description: "This lesson we will discussion about environment, pollution and global warnming", img: "/environment.png"},
+        {title: "Environment3", description: "This lesson we will discussion about environment, pollution and global warnming", img: "/environment.png"},
+        {title: "Environment4", description: "This lesson we will discussion about environment, pollution and global warnming", img: "/environment.png"},
+        {title: "Environment5", description: "This lesson we will discussion about environment, pollution and global warnming", img: "/environment.png"},
+        {title: "Environment6", description: "This lesson we will discussion about environment, pollution and global warnming", img: "/environment.png"},
+        {title: "Environment7", description: "This lesson we will discussion about environment, pollution and global warnming", img: "/environment.png"},
+        {title: "Environment8", description: "This lesson we will discussion about environment, pollution and global warnming", img: "/environment.png"},
+        {title: "Environment9", description: "This lesson we will discussion about environment, pollution and global warnming", img: "/environment.png"}
+    ];
+    const pageCount = Math.ceil(listTopic.length / topicsPerPage);
+
+    const handlePageClick = (event) => {
+      setCurrentPage(event.selected);
+    };
+  
+    const displayedTopics = listTopic.slice(currentPage * topicsPerPage, (currentPage + 1) * topicsPerPage);
+  
+    return (
+        <Stack direction="column">
+            <MainPicture src="/bg_vocabulary.png" title="Vocabulary"/>
+            <Typography variant="h5" component="p" sx={{ padding: '1rem', marginX: '5%', marginY: '1rem' }}>
+                Expand your knowledge and enhance your skills through our diverse vocabulary topics. Explore and learn with us!
+            </Typography>
+            <ListTopicContent listTopic={displayedTopics} />
+
+            <ReactPaginate
+                previousLabel={"Previous"}
+                nextLabel={"Next"}
+                breakLabel={"..."}
+                pageCount={pageCount}
+                marginPagesDisplayed={2}
+                pageRangeDisplayed={5}
+                onPageChange={handlePageClick}
+                containerClassName={"pagination"}
+                activeClassName={"active"}
+            />
+        </Stack>
+    );
 }
 
 export default ListTopic;
---
+++ src/components/student/vocabulary/MainPicture.jsx
@@ -0,0 +1,37 @@
+import { Stack, Typography } from "@mui/material";
+
+function MainPicture({ src, title }) {
+  return (
+    <Stack 
+      direction="column" 
+      alignItems="center" 
+      justifyContent="center"
+      sx={{ position: 'relative', height: '100%', width: '100%' }}
+    >
+      <img 
+        src={src} 
+        alt="main" 
+        style={{ width: '100%', height: 'auto', objectFit: 'cover' }} 
+      />
+      <Typography 
+        variant="h4" 
+        component="h2" 
+        sx={{
+          position: 'absolute',
+          fontWeight: 'bold',
+          bottom: '0',
+          left: '0',
+          color: '#000',
+          background: 'linear-gradient(90deg, rgba(255,255,255,1) 0%, rgba(255,255,255,0.4) 100%)',
+          paddingY: '1rem',
+          paddingX: '1.5rem',
+          borderRadius: '0.5rem',
+        }}
+      >
+        {title}
+      </Typography>
+    </Stack>
+  );
+}
+
+export default MainPicture;
