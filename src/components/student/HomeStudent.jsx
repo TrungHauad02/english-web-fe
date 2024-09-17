5a60b5720f33f18787dee92cab5b56fce17a8ade
Nguyễn Trung Hậu <trunghauad02@gmail.com>
2024-09-17 17:41:52 +0700

    GrammarInfo, GrammarDocs, PDFViewer

--- src/components/student/HomeStudent.jsx
+++ src/components/student/HomeStudent.jsx
@@ -13,45 +13,51 @@ import TestReading from "./common/Test/TestReading";
 import TestListening from "./common/Test/TestListening";
 import TestWriting from "./common/Test/TestWriting";
 import TestSpeaking from "./common/Test/TestSpeaking";
+import Grammar from "./grammar/Grammar";
 
-
-
-function HomeStudent(){
-    const componentMap = {
-        'ListTopic': ListTopic,
-        'ListTest': ListTest,
-        'ListTest_Skill': ListTest_Skill,
-        'TestReading': TestReading,
-        'TestListening': TestListening,
-        'TestSpeaking': TestSpeaking,
-       
-    };
-    return (
-        <Stack direction="column" justifyContent="space-between" sx={{ height: '100%' }}>
-            <HeaderStudent/>
-            <Stack direction="column" flexGrow={1} justifyContent="flex-start" sx={{ overflowY: 'auto' }}>
-                <Routes>
-                {
-                    site.map((page) => (
-                        <Route 
-                            key={page.id} 
-                            path={page.path} 
-                            element={React.createElement(componentMap[page.file], {
-                                list: page.list,
-                                bg: page.bg,
-                                title: page.title,
-                                quote: page.quote,
-                            })} 
-                        />
-                    ))
-                }
-                </Routes>
-                <Outlet />
-            </Stack>
-            <Footer/>
-        </Stack>
-    )
-
+function HomeStudent() {
+  const componentMap = {
+    ListTopic: ListTopic,
+    ListTest: ListTest,
+    ListTest_Skill: ListTest_Skill,
+    TestReading: TestReading,
+    TestListening: TestListening,
+    TestSpeaking: TestSpeaking,
+  };
+  return (
+    <Stack
+      direction="column"
+      justifyContent="space-between"
+      sx={{ height: "100%" }}
+    >
+      <HeaderStudent />
+      <Stack
+        direction="column"
+        flexGrow={1}
+        justifyContent="flex-start"
+        sx={{ overflowY: "auto" }}
+      >
+        <Routes>
+          {site.map((page) => (
+            <Route
+              key={page.id}
+              path={page.path}
+              element={React.createElement(componentMap[page.file], {
+                list: page.list,
+                bg: page.bg,
+                title: page.title,
+                quote: page.quote,
+              })}
+            />
+          ))}
+          <Route path="/list-topic/1" element={<Vocabulary />} />
+          <Route path="/grammar/1" element={<Grammar />} />
+        </Routes>
+        <Outlet />
+      </Stack>
+      <Footer />
+    </Stack>
+  );
 }
 
 export default HomeStudent;
--- src/components/student/grammar/Grammar.jsx
+++ src/components/student/grammar/Grammar.jsx
@@ -1,11 +1,31 @@
 import { Stack } from "@mui/material";
+import MainPicture from "../common/MainPicture";
+import GrammarInfo from "./GrammarInfo";
+import GrammarDocument from "./GrammarDocument";
 
-function Grammar() {
+function Grammar({ topic }) {
+  topic = {
+    title: "Simple Present tense",
+    img: "/environment.png",
+  };
+
+  const grammar = {
+    id: 1,
+    title: "Simple Present tense",
+    img: "/environment.png",
+    content:
+      "Simple present tense is used to describe habits, unchanging situations, general truths, and fixed arrangements. It is also used with timetables, schedules, and events.",
+    example: "I play football every weekend.",
+    file: "/PresentContinuousTense.pdf",
+  };
+
   return (
-    <Stack spacing={2}>
-      <h4>Grammar</h4>
-    </Stack>
+    <>
+      <MainPicture title={topic.title} src={topic.img} />
+      <GrammarInfo grammar={grammar} />
+      <GrammarDocument file={grammar.file} title={grammar.title} />
+    </>
   );
 }
 
-export default ListGrammar;
+export default Grammar;
