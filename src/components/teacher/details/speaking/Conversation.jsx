dfe4769db1966b47cacec0251d59884a13cddee9
Nguyễn Trung Hậu <trunghauad02@gmail.com>
2024-10-03 19:23:36 +0700

    Teacher Speaking

---
+++ src/components/teacher/details/speaking/Conversation.jsx
@@ -0,0 +1,157 @@
+import {
+  Button,
+  FormControl,
+  Grid2,
+  MenuItem,
+  Select,
+  TextField,
+  Typography,
+} from "@mui/material";
+import EditButton from "../common/button/EditButton";
+import SaveButton from "../common/button/SaveButton";
+import { useState } from "react";
+import DeleteButton from "../common/button/DeleteButton";
+import { v4 as uuidv4 } from "uuid";
+
+export default function Conversation({
+  listPeople,
+  conversation,
+  setConversation,
+}) {
+  const [isEditing, setIsEditing] = useState(false);
+
+  const hanleAddNewLine = () => {
+    const newConversation = [
+      ...conversation,
+      {
+        id: uuidv4(),
+        name: listPeople[0],
+        serial: conversation.length + 1,
+        content: "",
+      },
+    ];
+    setConversation(newConversation);
+  };
+
+  return (
+    <Grid2 container direction={"column"} sx={{ width: "100%" }}>
+      <Grid2
+        container
+        direction={"row"}
+        justifyContent={"space-between"}
+        sx={{
+          position: "sticky",
+          top: 0,
+          backgroundColor: "#FFF4CC",
+          padding: "1rem 1rem",
+          zIndex: 3,
+          boxShadow: "0 0 0.5rem 0.1rem #00000030",
+        }}
+      >
+        <Grid2 item>
+          <Typography variant="h6">Conversation</Typography>
+        </Grid2>
+        <Grid2 container direction={"row"} spacing={4}>
+          <EditButton showText onedit={() => setIsEditing(true)} />
+          <SaveButton showText ondel={() => setIsEditing(false)} />
+        </Grid2>
+      </Grid2>
+      <Grid2
+        container
+        direction={"column"}
+        spacing={2}
+        sx={{
+          width: "100%",
+          padding: "1rem 1rem",
+        }}
+      >
+        {conversation.map((line, index) => (
+          <Line
+            key={index}
+            listPeople={listPeople}
+            line={line}
+            disabled={!isEditing}
+            conversation={conversation}
+            setConversation={setConversation}
+          />
+        ))}
+        <Button
+          onClick={hanleAddNewLine}
+          disabled={!isEditing}
+          sx={{
+            textDecoration: "underline",
+            fontStyle: "italic",
+            color: "#000",
+          }}
+        >
+          Add New Line
+        </Button>
+      </Grid2>
+    </Grid2>
+  );
+}
+
+function Line({ listPeople, line, disabled, conversation, setConversation }) {
+  const sx = {};
+  const onChangeName = (event) => {
+    const newConversation = conversation.map((item) => {
+      if (item.id === line.id) {
+        return { ...item, name: event.target.value };
+      }
+      return item;
+    });
+    setConversation(newConversation);
+  };
+
+  const onChangeContent = (event) => {
+    const newConversation = conversation.map((item) => {
+      if (item.id === line.id) {
+        return { ...item, content: event.target.value };
+      }
+      return item;
+    });
+    setConversation(newConversation);
+  };
+
+  const onDeleteLine = () => {
+    const newConversation = conversation.filter((item) => item.id !== line.id);
+    setConversation(newConversation);
+  };
+
+  return (
+    <Grid2 container direction={"row"} spacing={2}>
+      <Grid2 item size={2}>
+        <FormControl fullWidth sx={{ backgroundColor: "#fff" }}>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={line.name}
+            onChange={onChangeName}
+            disabled={disabled}
+            sx={sx}
+          >
+            {listPeople.map((option) => (
+              <MenuItem key={option} value={option}>
+                <Typography textTransform={"capitalize"}>{option}</Typography>
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </Grid2>
+      <Grid2 container direction={"row"} size={10} alignItems={"center"}>
+        <Grid2 item size={10}>
+          <TextField
+            value={line.content}
+            onChange={onChangeContent}
+            sx={{ backgroundColor: "#fff" }}
+            disabled={disabled}
+            fullWidth
+          />
+        </Grid2>
+        <Grid2 item size={2}>
+          <DeleteButton ondel={onDeleteLine} disabled={disabled} />
+        </Grid2>
+      </Grid2>
+    </Grid2>
+  );
+}
--- src/components/teacher/details/speaking/SpeakingDetail.jsx
+++ src/components/teacher/details/speaking/SpeakingDetail.jsx
@@ -1,7 +1,87 @@
+import { useParams } from "react-router-dom";
+import {
+  getSpeakingDetail,
+  getConversationDetail,
+} from "../../../../api/teacher/detailManagerment";
+import { Grid2 } from "@mui/material";
+import SpeakingInfo from "./SpeakingInfo";
+import { useState } from "react";
+import PeopleInConversation from "./PeopleInConversation";
+import Conversation from "./Conversation";
+
 export default function SpeakingDetail() {
+  const { id } = useParams();
+  const data = getSpeakingDetail(id);
+  const [localData, setLocalData] = useState(data);
+
+  const fakeData = ["John", "Dutch", "Mica", "Jenny", "Linda"];
+  const [people, setPeople] = useState(fakeData);
+
+  const conversationData = getConversationDetail(1) || [];
+  const [conversation, setConversation] = useState(conversationData);
+
   return (
-    <div>
-      <h1>Speaking Detail</h1>
-    </div>
+    <Grid2 container direction={"row"} sx={{ margin: "2rem 1% 2rem 2%" }}>
+      <Grid2 item size={5}>
+        {/** Reading info*/}
+        <SpeakingInfo data={localData} setData={setLocalData} />
+      </Grid2>
+      <Grid2 container size={7} direction={"column"} spacing={4}>
+        <Grid2
+          container
+          boxShadow={"0 0 0.5rem 0.1rem #00000040"}
+          sx={{
+            backgroundColor: "#FFF4CC",
+            borderRadius: "0.5rem",
+            height: "50vh",
+            overflowY: "auto",
+            "&::-webkit-scrollbar": {
+              width: "0.5rem",
+            },
+            "&::-webkit-scrollbar-track": {
+              backgroundColor: "#e0e0e0",
+            },
+            "&::-webkit-scrollbar-thumb": {
+              backgroundColor: "#888",
+              borderRadius: "10px",
+            },
+            "&::-webkit-scrollbar-thumb:hover": {
+              backgroundColor: "#555",
+            },
+          }}
+        >
+          <PeopleInConversation listPeople={people} setListPeople={setPeople} />
+        </Grid2>
+        <Grid2
+          container
+          boxShadow={"0 0 0.5rem 0.1rem #00000040"}
+          sx={{
+            backgroundColor: "#FFF4CC",
+            borderRadius: "0.5rem",
+            height: "72vh",
+            overflowY: "auto",
+            "&::-webkit-scrollbar": {
+              width: "0.5rem",
+            },
+            "&::-webkit-scrollbar-track": {
+              backgroundColor: "#e0e0e0",
+            },
+            "&::-webkit-scrollbar-thumb": {
+              backgroundColor: "#888",
+              borderRadius: "10px",
+            },
+            "&::-webkit-scrollbar-thumb:hover": {
+              backgroundColor: "#555",
+            },
+          }}
+        >
+          <Conversation
+            listPeople={people}
+            conversation={conversation}
+            setConversation={setConversation}
+          />
+        </Grid2>
+      </Grid2>
+    </Grid2>
   );
 }
