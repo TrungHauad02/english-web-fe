4737bbde5fc902259f8c3c783b71918ed1fc1cf5
Nguyễn Trung Hậu <trunghauad02@gmail.com>
2024-10-29 17:06:42 +0700

    listen answer question in student

---
+++ src/components/student/skill/listening/useListenAndAnswerQuestion.js
@@ -0,0 +1,27 @@
+import { getAnswerQuestions } from "api/study/answerQuestion/answerQuestionService";
+import { getListeningDetail } from "api/study/listening/listeningService";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+export default function useListenAndAnswerQuestion() {
+  const [listening, setListening] = useState(null);
+  const [listQuestion, setListQuestion] = useState(null);
+  const { id } = useParams();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const [data, listQuestionData] = await Promise.all([
+        getListeningDetail(id),
+        getAnswerQuestions("listening", id),
+      ]);
+      setListening(data);
+      setListQuestion(listQuestionData);
+    };
+    fetchData();
+  }, [id]);
+
+  return {
+    listening,
+    listQuestion,
+  };
+}
