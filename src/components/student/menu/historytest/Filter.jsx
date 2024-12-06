ca31fba20fb0f853048e6d2e2a3444368fbbfca5
CapTan2003 <113422566+CapTan2003@users.noreply.github.com>
2024-12-06 07:21:41 +0700

    cập nhật sửa sort theo serial và status test và reset filter history test

--- src/components/student/menu/historytest/Filter.jsx
+++ src/components/student/menu/historytest/Filter.jsx
@@ -1,37 +1,53 @@
 import React from "react";
 import { TextField, Button, MenuItem, Grid, IconButton } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
-import { Search } from '@mui/icons-material';
-import { format } from 'date-fns';
+import { Search, Replay } from "@mui/icons-material";
+import { format } from "date-fns";
 import useColor from "shared/color/Color";
 
-const Filter = ({ filter, setFilter, searchText, setSearchText, searchStartDate, setSearchStartDate, searchEndDate, setSearchEndDate, handleSearch}) => {
+const Filter = ({
+    filter,
+    setFilter,
+    searchText,
+    setSearchText,
+    searchStartDate,
+    setSearchStartDate,
+    searchEndDate,
+    setSearchEndDate,
+    handleSearch
+}) => {
     const isEndDateDisabled = !searchStartDate || new Date(searchStartDate) >= new Date();
     const { Color1, Color2, Color2_1, Color3, Color4, HeaderBg } = useColor();
+
     const formatStartOfDay = (date) => {
         if (!date) return "";
         const newDate = new Date(date);
         newDate.setHours(0, 0, 0, 0);
-        return `${newDate.getFullYear()}-${String(newDate.getMonth() + 1).padStart(2, '0')}-${String(newDate.getDate()).padStart(2, '0')} ${String(newDate.getHours()).padStart(2, '0')}:${String(newDate.getMinutes()).padStart(2, '0')}:${String(newDate.getSeconds()).padStart(2, '0')}`;
-    };
-    const formatDateForInput = (date) => {
-        if (!date) return "";
-        return format(new Date(date), 'yyyy-MM-dd'); 
+        return `${newDate.getFullYear()}-${String(newDate.getMonth() + 1).padStart(2, "0")}-${String(newDate.getDate()).padStart(2, "0")} ${String(newDate.getHours()).padStart(2, "0")}:${String(newDate.getMinutes()).padStart(2, "0")}:${String(newDate.getSeconds()).padStart(2, "0")}`;
     };
 
+    const formatDateForInput = (date) => {
+        if (!date) return "";
+        return format(new Date(date), "yyyy-MM-dd");
+    };
 
     const formatEndOfDay = (date) => {
         if (!date) return "";
         const newDate = new Date(date);
         newDate.setHours(23, 59, 59, 999);
-        return `${newDate.getFullYear()}-${String(newDate.getMonth() + 1).padStart(2, '0')}-${String(newDate.getDate()).padStart(2, '0')} ${String(newDate.getHours()).padStart(2, '0')}:${String(newDate.getMinutes()).padStart(2, '0')}:${String(newDate.getSeconds()).padStart(2, '0')}`;
+        return `${newDate.getFullYear()}-${String(newDate.getMonth() + 1).padStart(2, "0")}-${String(newDate.getDate()).padStart(2, "0")} ${String(newDate.getHours()).padStart(2, "0")}:${String(newDate.getMinutes()).padStart(2, "0")}:${String(newDate.getSeconds()).padStart(2, "0")}`;
     };
 
-    
+    const handleResetFilter = () => {
+        setSearchText("");
+        setFilter("ALL");
+        setSearchStartDate("");
+        setSearchEndDate("");
+    };
 
     return (
         <Grid container spacing={1} alignItems="center" mb={2}>
-            <Grid item xs={12} sm={4}>
+            <Grid item xs={12} sm={3}>
                 <TextField
                     fullWidth
                     variant="outlined"
@@ -66,7 +82,7 @@ const Filter = ({ filter, setFilter, searchText, setSearchText, searchStartDate,
                     variant="outlined"
                     type="date"
                     InputLabelProps={{ shrink: true }}
-                    value={(formatDateForInput(searchStartDate))}
+                    value={formatDateForInput(searchStartDate)}
                     onChange={(e) => {
                         const formattedStartDate = formatStartOfDay(e.target.value);
                         setSearchStartDate(formattedStartDate);
@@ -75,12 +91,13 @@ const Filter = ({ filter, setFilter, searchText, setSearchText, searchStartDate,
                         }
                     }}
                     onKeyDown={(e) => {
-                        if (e.key === 'Enter') {
+                        if (e.key === "Enter") {
                             handleSearch();
                         }
                     }}
                 />
             </Grid>
+
             <Grid item xs={2}>
                 <TextField
                     fullWidth
@@ -88,7 +105,7 @@ const Filter = ({ filter, setFilter, searchText, setSearchText, searchStartDate,
                     variant="outlined"
                     type="date"
                     InputLabelProps={{ shrink: true }}
-                    value={(formatDateForInput(searchEndDate))}
+                    value={formatDateForInput(searchEndDate)}
                     onChange={(e) => {
                         const formattedEndDate = formatEndOfDay(e.target.value);
                         if (new Date(formattedEndDate) >= new Date(searchStartDate)) {
@@ -97,22 +114,33 @@ const Filter = ({ filter, setFilter, searchText, setSearchText, searchStartDate,
                     }}
                     disabled={isEndDateDisabled}
                     onKeyDown={(e) => {
-                        if (e.key === 'Enter') {
+                        if (e.key === "Enter") {
                             handleSearch();
                         }
                     }}
                 />
             </Grid>
             <Grid item xs={1}>
+                <IconButton
+                    color="secondary"
+                    onClick={handleResetFilter}
+                    sx={{ marginTop: ".5rem" }}
+                >
+                    <Replay />
+                </IconButton>
+            </Grid>
+
+            <Grid item xs={1}>
                 <Button
                     variant="contained"
                     startIcon={<Search />}
                     onClick={handleSearch}
-                    sx={{ marginTop: '.5rem', background: Color2_1 }}
+                    sx={{ marginTop: ".5rem", background: Color2_1 }}
                 >
                     Search
                 </Button>
             </Grid>
+           
         </Grid>
     );
 };
