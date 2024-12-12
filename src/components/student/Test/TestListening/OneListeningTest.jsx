d15636ccac846b54ca353f87ee3b6c4376119822
CapTan2003 <113422566+CapTan2003@users.noreply.github.com>
2024-12-12 10:56:30 +0700

    chỉnh test listening khi f5 audio quay lại như cũ

--- src/components/student/Test/TestListening/OneListeningTest.jsx
+++ src/components/student/Test/TestListening/OneListeningTest.jsx
@@ -1,10 +1,16 @@
 
 import ListQuestion from '../common/ListQuestion'
-
-function OneListeningTest({ oneListening, audioRef, onAudioEnd ,title,answers,setAnswers}) {
+import React, { useEffect } from "react";
+function OneListeningTest({ oneListening, audioRef, onAudioEnd ,title,answers,setAnswers,currentAudioTime}) {
+  useEffect(() => {
+    if (audioRef.current) {
+      audioRef.current.currentTime = currentAudioTime;
+      audioRef.current.play();
+    }
+  }, [oneListening]);
   return (
     <>
-      <audio ref={audioRef} src={oneListening.content} onEnded={onAudioEnd} autoPlay  
+      <audio ref={audioRef} src={oneListening.content} onEnded={onAudioEnd} 
      />  
       <ListQuestion 
     dataTest= {oneListening} answers = {answers}
